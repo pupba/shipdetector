1238e06b0636bb63aab0829e70eb8df7d181b321
S_hine2 <rhdk5148@gmail.com>
2023-06-03 20:17:47 +0900

    add modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,7 @@
 """Ship Detect & Tracking Package"""
 from modules.ship_detect import *
 from modules.decision import *
+from modules.hardware import *
+from modules.lidar import *
 
-__all__ = ['ship_detect','decision']
+__all__ = ['ship_detect','decision','hardware','lidar']
